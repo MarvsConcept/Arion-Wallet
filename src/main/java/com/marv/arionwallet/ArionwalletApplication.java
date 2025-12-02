7332659be205b8060ad68257863ff4716265b544
MarvsConcept <Marvade8@gmail.com>
2025-12-01 22:22:00 -0800

    Setup and Initial Commit

---
+++ src/main/java/com/marv/arionwallet/ArionwalletApplication.java
@@ -0,0 +1,13 @@
+package com.marv.arionwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArionwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArionwalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marv/arionwallet/ArionwalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marv.arionwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArionwalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
