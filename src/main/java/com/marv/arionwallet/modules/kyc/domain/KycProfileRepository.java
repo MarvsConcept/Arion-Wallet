1ac368a01d036fd3f15db89b0ba861d0a3fc0889
MarvsConcept <Marvade8@gmail.com>
2026-02-16 17:03:36 -0800

    Added Repo to list pending kycprofiles

--- src/main/java/com/marv/arionwallet/modules/kyc/domain/KycProfileRepository.java
+++ src/main/java/com/marv/arionwallet/modules/kyc/domain/KycProfileRepository.java
@@ -1,5 +1,8 @@
 package com.marv.arionwallet.modules.kyc.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.util.Optional;
 import java.util.UUID;
 
@@ -7,5 +10,6 @@ public interface KycProfileRepository {
 
     KycProfile save(KycProfile profile);
     Optional<KycProfile> findByUserId(UUID userId);
+    Page<KycProfile> findByStatusOrderBySubmittedAtAsc(KycStatus status, Pageable pageable);
     boolean existsByUserId(UUID userId);
 }
